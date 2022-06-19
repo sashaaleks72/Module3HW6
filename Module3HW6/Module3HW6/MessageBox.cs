b78e4519cb7cb9cc722ac79665aedcd40fa18558
sashaaleks72 <sashaaleks72@gmail.com>
2022-06-19 14:33:59 +0300

    Implemented all tasks described in paragraphes

---
+++ Module3HW6/Module3HW6/MessageBox.cs
@@ -0,0 +1,31 @@
+﻿namespace Module3HW6
+{
+    public class MessageBox
+    {
+        public enum State
+        {
+            Ok,
+            Cancel
+        }
+
+        private event Action<State> OnWindowClosed;
+
+        public MessageBox(Action<State> IsClosedHandler)
+        {
+            OnWindowClosed += IsClosedHandler;
+        }
+
+        public async void Open()
+        {
+            State state = (State)new Random().Next(0, 2);
+
+            Console.WriteLine("Window is open");
+
+            await Task.Delay(3000);
+
+            Console.WriteLine("Window was closed by a user");
+
+            OnWindowClosed(state);
+        }
+    }
+}
